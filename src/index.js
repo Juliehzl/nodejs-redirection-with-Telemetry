44b89a65b352ef4e37aad57eae1f8062fc7f1ba7
Zunli Hu <zuh@microsoft.com>
2019-06-18 18:47:58 +0800

    Change http to https

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const http = require('http');
 const telemetry = require('vscode-extension-telemetry-wrapper');
 
 const handler = (oid, req, res) => {
-    const version = process.env.version;
-    //const version = "0.1.0";
-
-    //Analyze request headers and send info
     try {
+    //const version = process.env.version;
+        const version = "0.1.0";
+        //Analyze request headers and send info
+    
         const requestHeaders = req.headers;
-        const {referer} = requestHeaders;
-        const ip = req.headers['x-forwarded-for'];
+        const referer = "";
+        if ("referer" in Object.keys(requestHeaders))
+            referer = req.headers["referer"];
+        const ip = req.headers['x-forwarded-for'] || "";
         let ipAddress = ip.split(":")[0];
         const geo = geoip.lookup(ipAddress);
         const country = geo == null ? null : geo.country;
@@ -21,31 +23,37 @@ const handler = (oid, req, res) => {
         telemetry.sendInfo(oid, {
             name:"requestInfo",
             "user-agent": requestHeaders["user-agent"],
-            referer,
+            "referer": referer,
             "downloadVersion": version,
             "accept-language": requestHeaders["accept-language"],
             "country": country,
             "region": region,
             "city": city,
         }, {});
-    } catch (err) {
-        console.log(err);
-    }
 
-    // Parse request url
-    let parsedUrl= req.url.split("/");
-    if(parsedUrl[parsedUrl.length - 1] == "")
-        parsedUrl.pop();
-    const reqInstaller = parsedUrl[parsedUrl.length - 1];
-    let installerInfo = reqInstaller.split("-");
-    
-    const oslist = ["win"];
-    if (installerInfo.length == 2) { 
-        const os = installerInfo[1];
-        if(oslist.includes(os))
-            res.writeHead(302, {
-                    location: `https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJavaInstaller-online-${os}-${version}.exe`});
-        else {
+        // Parse request url
+        let parsedUrl= req.url.split("/");
+        if(parsedUrl[parsedUrl.length - 1] == "")
+            parsedUrl.pop();
+        const reqInstaller = parsedUrl[parsedUrl.length - 1];
+        let installerInfo = reqInstaller.split("-");
+        
+        const oslist = ["win"];
+        if (installerInfo.length == 2) { 
+            const os = installerInfo[1];
+            if(oslist.includes(os)) {
+                res.writeHead(302, {
+                   location: `https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJavaInstaller-online-${os}-${version}.exe`});
+            }
+            else {
+                const err = new Error("No request handler found for " + req.url);
+                res.writeHead(404, {"Content-Type": "text/plain"});
+                res.write("404 Not found");
+                res.end();
+                telemetry.setUserError(err);
+                throw(err);
+            }
+        } else { // Unvalid format
             const err = new Error("No request handler found for " + req.url);
             res.writeHead(404, {"Content-Type": "text/plain"});
             res.write("404 Not found");
@@ -53,20 +61,26 @@ const handler = (oid, req, res) => {
             telemetry.setUserError(err);
             throw(err);
         }
-    } else { // Unvalid format
-        const err = new Error("No request handler found for " + req.url);
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.write("404 Not found");
-        res.end();
-        telemetry.setUserError(err);
-        throw(err);
+        return res.end();
+    } catch (err) {
+        console.log(err);
     }
-    return res.end();
 };
 
 const initialized = telemetry.initializeFromJsonFile("./package.json", true);
 const instrumentedHandler = telemetry.instrumentOperation("download", (oid, ...args) => handler(oid, ...args));
 const server = http.createServer(instrumentedHandler);
+server.on('request', (request, response) => {
+    const { method, url } = request;
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    // at this point, `body` has the entire request body stored in it as a string
+    });
+});
+
 const port = process.env.PORT || 1337;
 
 server.listen(port);
